eac6de6e5255770498c775100f816d6ca5249b64
Daniel Shin <danielshin234@gmail.com>
2018-11-07 16:48:59 -0800

    init

---
+++ cmake-build-debug/CMakeFiles/Lab7.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/a00257049/CLionProjects/Lab7/Lab7/Course.cpp" "/cygdrive/c/Users/a00257049/CLionProjects/Lab7/Lab7/cmake-build-debug/CMakeFiles/Lab7.dir/Course.cpp.o"
+  "/cygdrive/c/Users/a00257049/CLionProjects/Lab7/Lab7/main.cpp" "/cygdrive/c/Users/a00257049/CLionProjects/Lab7/Lab7/cmake-build-debug/CMakeFiles/Lab7.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab7.dir/main.cpp.o"
+  "CMakeFiles/Lab7.dir/Course.cpp.o"
+  "Lab7.pdb"
+  "Lab7.exe"
+  "libLab7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
